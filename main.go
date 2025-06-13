16561cc6967b3bc50bd03b472113aa0cda634748
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2025-06-13 16:44:09 +0200

    Add simplecontroller

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/klog/v2/textlogger"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+const (
+	watchKey   = "simplecontroller.io/watch"
+	watchValue = "true"
+
+	colorKey   = "simplecontroller.io/color"
+	colorValue = "blue"
+)
+
+type PodLabelReconciler struct {
+	client.Client
+}
+
+func (r *PodLabelReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	var pod corev1.Pod
+
+	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if pod.Labels[watchKey] != watchValue {
+		return ctrl.Result{}, nil
+	}
+
+	if pod.Labels[colorKey] == colorValue {
+		return ctrl.Result{}, nil
+	}
+
+	updated := pod.DeepCopy()
+	updated.Labels[colorKey] = colorValue
+
+	return ctrl.Result{}, r.Patch(ctx, updated, client.MergeFrom(&pod))
+}
+
+func (r *PodLabelReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).For(&corev1.Pod{}).WithEventFilter(predicate.NewPredicateFuncs(func(o client.Object) bool {
+		return o.GetLabels()[watchKey] == watchValue
+	})).Complete(r)
+}
+
+func main() {
+	loggerConfig := textlogger.NewConfig()
+	logger := textlogger.NewLogger(loggerConfig).WithName("simplecontroller")
+	ctrl.SetLogger(logger)
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := (&PodLabelReconciler{
+		Client: mgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		panic(err)
+	}
+
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		panic(err)
+	}
+}
